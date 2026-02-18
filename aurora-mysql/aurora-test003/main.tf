f07792327960971a996fe8b4d4327cf2e0690737
jimaoka <jimaoka@users.noreply.github.com>
2026-02-18 12:22:46 +0900

    Add aurora-mysql aurora-test003

---
+++ aurora-mysql/aurora-test003/main.tf
@@ -0,0 +1,18 @@
+module "aurora-test003" {
+  source = "../../modules/aurora-mysql"
+
+  cluster_identifier        = "aurora-test003"
+  db_subnet_group_name      = "shared-dev-rds-db-subnet-group"
+  region                    = var.aws_region
+  db_cluster_instance_class = "db.t4g.medium"
+  engine_version            = "8.0.mysql_aurora.3.10.3"
+  read_replica_count        = 0
+  deletion_protection       = false
+
+  backup_retention_period      = 1
+  preferred_backup_window      = "18:00-19:00"
+  preferred_maintenance_window = "sun:19:00-sun:20:00"
+  backtrack_window             = 0
+
+  db_parameters = []
+}
